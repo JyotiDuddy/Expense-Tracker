80ccde67b2488446b3f98d1514893f7c9d1bcb36
JyotiDuddy <jyotiduddy00@gmail.com>
2024-05-20 19:43:03 +0530

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseTracker from "./components/ExpenseTracker";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <ExpenseTracker />
+    </div>
+  );
+}
